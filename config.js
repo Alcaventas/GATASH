9e56022ba5db45c0eeca1e329fc1e40d94d31240
Alcaventas <141533784+Alcaventas@users.noreply.github.com>
2024-10-24 00:32:40 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -43,8 +43,6 @@ global.confirmCode = "" // No tocar esto : Do not touch this line
 // Español 👉 es           
 // English 👉 en
 global.lenguajeGB = es
-global.mid = esDefault
-global.version_language = '1.0 (MID-GB)'
 
 // [ES] > Si "default_language" esta vacío, su idioma predeterminado será Español o se usará el idioma que cada usuario haya seleccionado al momento de registrarse. 
 // [EN] > If "default_language" is empty, your default language will be Spanish or the language that each user has selected at the time of registration will be used.
